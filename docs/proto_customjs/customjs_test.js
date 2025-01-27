ae4fa65bd5d809a10e66329d43cd52a3ab97f705
Veenus Adiyodi <veenusav@users.noreply.github.com>
2025-01-27 17:23:47 +0530

    proto customJS added

---
+++ docs/proto_customjs/customjs_test.js
@@ -0,0 +1,21 @@
+
+window.module = {
+
+    validate: (jsonobject, responseString) => {
+        return "simply Validated!"
+    },
+    // other functions...
+    getSampleString1: function () {
+        return "This is sample string 1";
+    },
+    getSampleString2: function () {
+        return "This is sample string 2";
+    },
+    getSampleString3: function () {
+        return "This is sample string 3";
+    },
+    getSampleString4: function () {
+        return "This is sample string 4";
+    },
+
+};
